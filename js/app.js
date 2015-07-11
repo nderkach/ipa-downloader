d92205220bd917556a0ce9f7d3980212eaebd815
LeoAJ <leo0924@gmail.com>
2015-07-10 17:42:18 -0700

    add gif

--- js/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from './components/App';
-import '../css/semantic.css';
-import '../css/app.css';
-
-React.render(
-  <App />,
-  document.querySelector('#itunes-search')
-);
